f928252141cf698bff9f7454ecc53cf14ee3b794
xuchongyu <1064207592@qq.com>
2021-10-27 10:11:39 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="toggleSelection(tableData[1])">切换第二行的选中状态</el-button>
+    <el-button @click="toggleSelection()">取消选择</el-button>
+    <div>{{ renderTime }}</div>
+    <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%">
+      <el-table-column type="selection" width="55"> </el-table-column>
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180">
+        <template slot-scope="params">
+          <div class="el-input">
+            <input
+              class="el-input__inner"
+              v-model.lazy="params.row.name"
+              placeholder="请输入内容"
+            />
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="地址">
+        <template slot-scope="params">
+          <el-select v-model.lazy="params.row.address" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      renderTime: null,
+      inputValue: '我爱我家',
+      tableData: [],
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕',
+        },
+        {
+          value: '选项2',
+          label: '双皮奶',
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎',
+        },
+        {
+          value: '选项4',
+          label: '龙须面',
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭',
+        },
+      ],
+    }
+  },
+  directive: {},
+  methods: {
+    toggleSelection(row) {
+      const s = window.performance.now()
+      if (row) {
+        this.$refs.multipleTable.toggleRowSelection(row)
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+      setTimeout(() => {
+        this.renderTime = (window.performance.now() - s).toFixed(2) + 'ms'
+      })
+    },
+  },
+  mounted() {
+    for (let i = 0; i < 2000; i++) {
+      this.tableData.push({
+        id: i,
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '选项1',
+      })
+    }
+  },
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import ELementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+console.log('ELementUI', ELementUI)
+Vue.use(ELementUI)
 new Vue({
-  render: function (h) { return h(App) },
+    render: function(h) {
+        return h(App)
+    },
 }).$mount('#app')
